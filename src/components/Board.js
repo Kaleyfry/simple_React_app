5302600dbf9842cc9f424f814f6594dfe6d2b2e0
Kaleyfry <59694186+Kaleyfry@users.noreply.github.com>
2020-05-14 19:45:53 -0400

    Build out base of app

---
+++ src/components/Board.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import '../css/Board.css'
+
+function Board() {
+    return (
+        <div>
+            <div className="div-board">
+                <div className="row">
+                </div>
+            </div>
+            <div>
+                <button className="btn btn-success add-button">Add</button>
+            </div>
+        </div>
+    )
+}
+
+export default Board;
